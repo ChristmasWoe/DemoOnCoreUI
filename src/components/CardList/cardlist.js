8bd61fd14f2f1dbafdbadeb9ab2a659f9bc90571
Всеволод@Annihilation <seva.pantiukhin@yandex.ru>
2020-07-23 00:19:07 +0400

    one line in cardlist //instage

--- src/components/CardList/cardlist.js
+++ src/components/CardList/cardlist.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import ItemCard from '../itemCard/itemcard'
+import "./cardlist.css"
 
 class CardList extends React.Component {
     constructor() {
         super();
+        this.state={
+            mode: "list"
+        }
     }
 
     render() {
         return (
             <div>
-                {
+                {this.state.mode=="tiles"?
                     this.props.items.map((item, index) => {
                         return (
                             <ItemCard key={index} name={item.name} description={item.description} imgsrc="https://picsum.photos/300/180" />
                         )
                     })
-                }
+                :
+                <div className="cardsInOneList"> 
+                {this.props.items.map((item, index) => {
+                        return (
+                            <ItemCard key={index} name={item.name} description={item.description} imgsrc="https://picsum.photos/300/180" />
+                        )
+                    })}
+                     </div>}
             </div >
         );
     }
